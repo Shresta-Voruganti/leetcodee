e5b4f6d976f6b0bb2e30f9da9a0033abc31debe6
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-10-10 03:33:22 +0530

    Time: 14 ms (25.11%), Space: 49.8 MB (71.51%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,21 @@
+import java.util.Hashtable;
+class Solution {
+    public int majorityElement(int[] nums) 
+    {
+        Hashtable<Integer,Integer> checkele = new Hashtable<>();
+        for(int ch : nums)
+        {
+            checkele.put(ch, checkele.getOrDefault(ch, 0)+1);
+        }
+        int n = nums.length;
+        int res = 0;
+        for(Map.Entry<Integer,Integer> e : checkele.entrySet())
+        {
+            if(e.getValue() > (int)n/2 )
+            {
+                res = e.getKey();
+            }
+        }
+        return res;
+    }
+}
