e698d4d0f3077b24348fb80e0a7ff57d600051e4
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-10-10 03:14:17 +0530

    Time: 588 ms (14.01%), Space: 0B (100%) - LeetHub

---
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+SELECT name FROM Customer WHERE referee_id!=2 OR referee_id IS NULL;
+-- SELECT name FROM Customer WHERE referee_id IS NULL OR referee_id!=2;
