e9e34619fb02037862fc57f0be667f54176ac387
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-10-11 21:03:08 +0530

    Time: 0 ms (100%), Space: 41.3 MB (98.06%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -12,8 +12,11 @@ class Solution {
             digits[i] = 0;
        }
 
+       // if all are 9's in the digits, so we need extra place to store 1
+
        int[] newdig = new int[n+1]; //elements are 0 by default
        newdig[0] = 1;
        return newdig;
+       
     }
 }
