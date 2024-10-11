b3951962971422e952ab8428c564e5ac7c6e7394
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-10-11 21:02:15 +0530

    Time: 0 ms (100%), Space: 41.4 MB (91.65%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
+       int n = digits.length;
 
-        for(int i=n-1; i>=0; i--)
-        {
+       for(int i=n-1; i>=0 ;i--)
+       {
             if(digits[i]<9)
             {
                 digits[i]++;
                 return digits;
             }
             digits[i] = 0;
-        }
+       }
 
-        int[] newin = new int[n+1];
-        newin[0] = 1;
-        return newin;
+       int[] newdig = new int[n+1]; //elements are 0 by default
+       newdig[0] = 1;
+       return newdig;
     }
 }
