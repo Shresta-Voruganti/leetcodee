46f5c0c40b17c917326c1d1b528435a946dd14bf
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-10-27 16:28:59 +0530

    Time: 0 ms (100%), Space: 41.2 MB (77.91%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,6 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs==null || strs.length==0)
+        if(strs==null || strs.length==0) //if the string is empty or if the string has no common prefix
         {
             return "";
         }
