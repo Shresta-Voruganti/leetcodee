bcfac51250c6b4ed016b93e0b596906b4900a35e
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-11-05 13:55:44 +0530

    Time: 26 ms (29.37%), Space: 46 MB (52.73%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String compressedString(String word) {
+        StringBuilder comp = new StringBuilder();
+        int count = 1;
+        for(int i=0; i<word.length(); i++) {
+            while(i<word.length()-1 && word.charAt(i)==word.charAt(i+1) && count<9) {
+                count++;
+                i++;
+            }
+            comp.append(String.valueOf(count));
+            comp.append(word.charAt(i));
+            count = 1;
+        }
+        return comp.toString();
+    }
+}
