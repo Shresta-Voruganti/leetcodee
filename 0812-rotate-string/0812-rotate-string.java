5fcd90f1dcbb758ae6259ef41a4ff2e583add71d
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-11-05 12:32:37 +0530

    Time: 0 ms (100%), Space: 41.3 MB (72.38%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -3,15 +3,7 @@ class Solution {
         if(s.length()!=goal.length()) {
             return false;
         }
-        if(s.equals(goal)) {
-            return true;
-        }
-        for(int i=0; i<s.length(); i++) {
-            s = s.substring(1)+s.charAt(0);
-            if(s.equals(goal)) {
-                return true;
-            }
-        }
-        return false;
+        String cc = s+s;
+        return cc.contains(goal);
     }
 }
