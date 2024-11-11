ba5211a8a65032a785453f14ef55b68184add956
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-11-11 21:16:38 +0530

    Time: 16 ms (43.61%), Space: 42.6 MB (18.35%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,29 +1,34 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length()) {
+        if(s.length() != t.length()) {
             return false;
         }
-        HashMap<Character,Character> mst = new HashMap<>();
-        HashMap<Character,Character> mts = new HashMap<>();
+        
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+        
         int n = s.length();
-        for(int i=0; i<n; i++) {
-            if(mst.containsKey(s.charAt(i))) {
-                if(mst.get(s.charAt(i))!=t.charAt(i)) {
-                    return false;
+        for(int i = 0; i < n; i++) {
+            char chS = s.charAt(i);
+            char chT = t.charAt(i);
+            
+            if(mapST.containsKey(chS)) {
+                if(mapST.get(chS) != chT) {
+                    return false; // s -> t mapping does not match
                 }
+            } else {
+                mapST.put(chS, chT);
             }
-            else {
-                mst.put(s.charAt(i),t.charAt(i));
-            }
-            if(mts.containsKey(t.charAt(i))) {
-                if(mts.get(t.charAt(i))!=s.charAt(i)) {
-                    return false;
+            
+            if(mapTS.containsKey(chT)) {
+                if(mapTS.get(chT) != chS) {
+                    return false; // t -> s mapping does not match
                 }
-            }
-            else {
-                mts.put(t.charAt(i),s.charAt(i));
+            } else {
+                mapTS.put(chT, chS);
             }
         }
+        
         return true;
     }
 }
