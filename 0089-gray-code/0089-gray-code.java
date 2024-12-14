ed1bf3b6e2cc76275145e5e20687886f328b107a
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-12-14 11:09:46 +0530

    Time: 2 ms (99.26%), Space: 51.4 MB (74.11%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,19 @@
+class Solution {
+    int x = 0;
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        backtrack(n,res);
+        return res;
+    }
+    public void backtrack(int n, List<Integer> res) {
+        if(n==0) {
+            res.add(x);
+            return;
+        }
+        else {
+            backtrack(n-1, res);
+            x = x ^ (1<<(n-1));
+            backtrack(n-1, res);
+        }
+    }
+}
