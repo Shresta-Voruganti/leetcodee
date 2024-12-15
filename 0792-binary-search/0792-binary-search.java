a96367ca8d4794c22b33afc0417fc324ae44336c
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-12-15 13:26:42 +0530

    Time: 0 ms (100%), Space: 45.7 MB (42.13%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,26 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        
-        while(start <= end)
-        {
-            int mid = start + ((end - start)/2);
-            if(target > nums[mid])
-            {
-                start = mid + 1;
-            }
-            else if(target < nums[mid])
-            {
-                end = mid - 1;
-            }
-            else
-            {
+        int low = 0;
+        int high = nums.length-1;
+
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            if(target==nums[mid]) {
                 return mid;
             }
+            else if(target>nums[mid]) {
+                low = mid+1;
+            }
+            else { //target<nums[mid]
+                high = mid-1;
+            }
         }
-
         return -1;
-
     }
 }
