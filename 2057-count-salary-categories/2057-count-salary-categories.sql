cd1099c3842f03e165e80e0ee02711a76b527cfc
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-12-18 01:08:38 +0530

    Time: 2162 ms (19.57%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -33,14 +33,19 @@ GROUP BY category;
 -- WHERE income > 50000;
 
 
--- SELECT category, COUNT(*) AS accounts_count
+-- don't use this approach
+-- SELECT category,
+--        COUNT(account_id) AS accounts_count
 -- FROM (
 --     SELECT 
 --         CASE 
 --             WHEN income < 20000 THEN 'Low Salary'
 --             WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
 --             WHEN income > 50000 THEN 'High Salary'
---         END AS category
+--         END AS category,
+--         account_id
 --     FROM Accounts
 -- ) AS categorized_accounts
--- GROUP BY category; 
+-- GROUP BY category
+-- ORDER BY FIELD(category, 'Low Salary', 'Average Salary', 'High Salary');
+
