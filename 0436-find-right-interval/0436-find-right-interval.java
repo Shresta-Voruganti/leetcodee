cb1c58b917feca8769ca07f6ff10ee6415a8df06
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-12-17 21:23:29 +0530

    Time: 11 ms (92.42%), Space: 49.8 MB (23.73%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -3,16 +3,28 @@ class Solution {
         int n = intervals.length;
         int[] res = new int[n];
 
+        int[][] indexedintervals = new int[n][3];
         for(int i=0; i<n; i++) {
-            int mindiff = Integer.MAX_VALUE;
+            indexedintervals[i][0] = intervals[i][0];
+            indexedintervals[i][1] = intervals[i][1];
+            indexedintervals[i][2] = i;
+        }
+
+        Arrays.sort(indexedintervals, (a,b) -> Integer.compare(a[0],b[0]));
+
+        for(int i=0; i<n; i++) {
+            int end = intervals[i][1];
+            int left = 0;
+            int right = n-1;
             int index = -1;
-            for(int j=0; j<n; j++) {
-                if(intervals[j][0] >= intervals[i][1]) {
-                    int diff = intervals[j][0] - intervals[i][1];
-                    if(diff < mindiff) {
-                        mindiff = diff;
-                        index = j;
-                    }
+            while(left<=right) {
+                int mid = left + (right-left)/2;
+                if(indexedintervals[mid][0] >= end) {
+                    index = indexedintervals[mid][2];
+                    right = mid-1; //to get small start
+                }                
+                else {
+                    left = mid+1; //to move right
                 }
             }
             res[i] = index;
