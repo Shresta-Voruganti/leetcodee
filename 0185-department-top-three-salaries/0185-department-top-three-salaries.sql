63c41e1d689d8b886fa2ac88d9c10857fd250f9d
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-12-18 21:18:45 +0530

    Time: 2004 ms (6.58%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,34 +1,13 @@
 # Write your MySQL query statement below
--- SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
--- FROM Employee e
--- JOIN Department d
--- ON e.departmentId = d.id
--- WHERE e.salary IN (SELECT DISTINCT e1.salary 
---     FROM Employee e1 
---     WHERE e1.departmentId = e.departmentId 
---     ORDER BY e1.salary DESC 
---     LIMIT 3)
 WITH ranked_employees AS (
     SELECT e.id, e.name, e.salary, e.departmentId,
            DENSE_RANK() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rank_e
     FROM Employee e
 )
-SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
-FROM ranked_employees e
-JOIN Department d ON e.departmentId = d.id
-WHERE e.rank_e <= 3
-ORDER BY d.name ASC, e.salary DESC;
+SELECT d.name AS Department, r.name AS Employee, r.salary AS Salary
+FROM ranked_employees r
+JOIN Department d ON r.departmentId = d.id
+WHERE r.rank_e <= 3
+ORDER BY d.name ASC, r.salary DESC;
 
 
--- WITH max_salaries AS (
---     SELECT DISTINCT e1.salary AS salary
---     FROM Employee e1
---     WHERE e1.departmentId = e.departmentId
---     ORDER BY e1.salary DESC
---     LIMIT 3
--- )
--- SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
--- FROM Employee e
--- JOIN Department d
--- ON e.departmentId = d.id
--- WHERE e.salary IN (SELECT salary FROM max_salaries);
