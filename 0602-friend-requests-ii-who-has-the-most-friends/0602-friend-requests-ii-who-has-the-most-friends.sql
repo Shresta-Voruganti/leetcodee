6883a45e94c220282074c71c4ce1c5a8b1d0a035
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-12-18 18:09:27 +0530

    Time: 721 ms (5.02%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 SELECT id, COUNT(id) AS num
-
 FROM (
     SELECT requester_id AS id
     FROM RequestAccepted
@@ -12,7 +11,6 @@ FROM (
     FROM RequestAccepted
     WHERE accepter_id IS NOT NULL 
 ) AS all_ids
-
 GROUP BY id
 ORDER BY num DESC
 LIMIT 1;
