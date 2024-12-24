14604e7a72eb3b4e354d8b6b381cf909773a3d6f
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-12-24 13:42:54 +0530

    Time: 1 ms (86.84%), Space: 40.8 MB (68.81%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,23 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x < 2) {
+        if (x == 0 || x == 1) {
             return x;
         }
-        long left = 1;
-        long right = x/2;
-        while(left <= right) {
-            long mid = left + (right - left)/2;
-            long square = mid * mid;
-            if(x == square) {
-                return (int) mid;
-            }
-            else if(x > square) { // target > arr[i] types
+        
+        int left = 1, right = x, result = 0;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // Avoid overflow by dividing first
+            if (mid <= x / mid) {
+                result = mid;
                 left = mid + 1;
-            }
-            else { // x < square // target < arr[i] types
+            } else {
                 right = mid - 1;
             }
         }
-        return (int) right;
+        
+        return result;
     }
 }
