40fbb63a69190ab1d673432beffa0c85721d19d6
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2024-12-27 14:53:26 +0530

    Time: 18 ms (81.08%), Space: 94.9 MB (19.83%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        int[] nums = new int[n];
-        for(int i=0; i<n; i++) {
-            nums[i] = i+1;
-        }
+        // int[] nums = new int[n];
+        // for(int i=0; i<n; i++) {
+        //     nums[i] = i+1;
+        // }
         List<List<Integer>> res = new ArrayList<>();
-        backtrack(nums, 0, res, k, new ArrayList<>());
+        backtrack(1, n, res, k, new ArrayList<>());
         return res;
     }
 
-    private void backtrack(int[] nums, int i, List<List<Integer>> res, int k, List<Integer> tempList) {
+    private void backtrack(int i, int n, List<List<Integer>> res, int k, List<Integer> tempList) {
         if(tempList.size()==k) {
             res.add(new ArrayList<>(tempList));
             return;
         }
 
-        for(int j=i; j<nums.length; j++) {
-            tempList.add(nums[j]);
-            backtrack(nums, j+1, res, k, tempList);
+        for(int j=i; j<=n; j++) {
+            tempList.add(j);
+            backtrack(j+1, n, res, k, tempList);
             tempList.remove(tempList.size()-1);
         }
     }
