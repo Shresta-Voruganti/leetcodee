2a62201dabb47376560d27757848f52b1a653f45
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-01-01 11:14:09 +0530

    Time: 1 ms (97.36%%), Space: 41.62 MB (36.08%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-spliting-a-stringgg2.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-spliting-a-stringgg2.java
@@ -1,1 +1,23 @@
-ii
+class Solution {
+    public int maxScore(String s) {
+        int ones = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i)=='1') {
+                ones++;
+            }
+        }
+        int zeros = 0;
+        int ans = 0;
+        for(int i=0; i<s.length()-1; i++) {
+            if(s.charAt(i)=='1') {
+                ones--;
+            }
+            else {
+                zeros++;
+            }
+            ans = Math.max(ans, zeros+ones);
+        }
+        return ans;
+    }
+}
+
