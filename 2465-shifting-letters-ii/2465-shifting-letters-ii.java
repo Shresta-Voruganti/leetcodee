118f723e69d902ff13c0f98dd22c0a4cbd83bc4b
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-01-06 23:42:25 +0530

    Time: 7 ms (75%), Space: 74.4 MB (56.13%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        int n = s.length();
+        int[] diffArray = new int[n];
+
+        for(int[] shift : shifts) {
+            if(shift[2] == 1) {
+                diffArray[shift[0]]++;
+                if(shift[1]+1 < n) {
+                    diffArray[shift[1] + 1]--;
+                }
+            }
+            else { //shift[2] == 0
+                diffArray[shift[0]]--;
+                if(shift[1]+1 < n) {
+                    diffArray[shift[1] + 1]++;
+                }
+            }
+        }
+
+        int numberOfShifts = 0;
+        StringBuilder result = new StringBuilder(s);
+        
+        for(int i = 0; i < n; i++) {
+            numberOfShifts = (numberOfShifts + diffArray[i]) % 26;
+            if(numberOfShifts < 0) {
+                numberOfShifts += 26;
+            }
+            char shiftedChar = (char) ('a' + ((s.charAt(i) - 'a' + numberOfShifts) % 26));
+            result.setCharAt(i, shiftedChar);
+        }
+        return result.toString();
+    }
+}
