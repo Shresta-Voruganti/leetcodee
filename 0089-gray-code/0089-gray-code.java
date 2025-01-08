cbda47cdd1e3177b3731e8da3720f3e739681f12
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-01-08 22:23:56 +0530

    Time: 2 ms (98.88%), Space: 51.7 MB (46.37%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -2,18 +2,19 @@ class Solution {
     int x = 0;
     public List<Integer> grayCode(int n) {
         List<Integer> res = new ArrayList<>();
-        backtrack(n,res);
+        backtrack(n, res);
         return res;
     }
-    public void backtrack(int n, List<Integer> res) {
-        if(n==0) {
+
+    private void backtrack(int n, List<Integer> res) {
+        if(n == 0) {
             res.add(x);
             return;
         }
         else {
-            backtrack(n-1, res);
-            x = x ^ (1<<(n-1));
-            backtrack(n-1, res);
+            backtrack(n - 1, res);
+            x = x ^ (1 << (n-1));
+            backtrack(n - 1, res);
         }
     }
 }
