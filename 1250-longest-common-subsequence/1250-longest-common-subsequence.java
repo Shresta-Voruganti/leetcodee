e5073eec62487c773e8c3bb1ff66909ecc0d7c4d
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-01-09 05:49:38 +0530

    Time: 21 ms (56.65%), Space: 50.7 MB (78.63%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -4,9 +4,23 @@ class Solution {
         int n = text2.length();
         int[][] dp = new int[m+1][n+1];
 
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                if(text1.charAt(i-1) == text2.charAt(j-1)) {
+        // for(int i = 1; i <= m; i++) {
+        //     for(int j = 1; j <= n; j++) {
+        //         if(text1.charAt(i-1) == text2.charAt(j-1)) {
+        //             dp[i][j] = 1 + dp[i-1][j-1];
+        //         }
+        //         else {
+        //             dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+        //         }
+        //     }
+        // }
+
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+                else if(text1.charAt(i-1) == text2.charAt(j-1)) {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else {
