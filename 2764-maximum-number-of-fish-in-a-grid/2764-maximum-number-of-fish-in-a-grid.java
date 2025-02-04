4a5a3005db88ccf457b95636bbc6a4725848dd8b
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-02-04 22:20:05 +0530

    Time: 5 ms (65.4%), Space: 44.8 MB (92.6%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
@@ -4,7 +4,6 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int maxfish = 0;
-        Queue<int[]> q = new LinkedList<>();
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
