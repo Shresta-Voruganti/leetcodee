f456692f31aaa8a3ecba50c3a4b65047fc76be75
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-02-05 17:06:39 +0530

    Time: 1 ms (52.21%), Space: 41.8 MB (39.72%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,36 +1,22 @@
-import java.util.Hashtable;
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
         if(s1.equals(s2)) {
             return true;
         }
 
-        Hashtable<Character, List<Integer>> ht1 = new Hashtable<>();
-        Hashtable<Character, List<Integer>> ht2 = new Hashtable<>();
-        List<Integer> diffIndices = new ArrayList<>();
+        List<Integer> diff = new ArrayList<>();
 
         for(int i = 0; i < s1.length(); i++) {
-            char ch1 = s1.charAt(i);
-            char ch2 = s2.charAt(i);
-
-            ht1.putIfAbsent(ch1, new ArrayList<>(List.of(0)));
-            ht1.get(ch1).set(0, ht1.get(ch1).get(0) + 1);
-            ht1.get(ch1).add(i);
-
-            ht2.putIfAbsent(ch2, new ArrayList<>(List.of(0)));
-            ht2.get(ch2).set(0, ht2.get(ch2).get(0) + 1);
-            ht2.get(ch2).add(i);
-
-            if(ch1 != ch2) {
-                diffIndices.add(i);
+            if(s1.charAt(i) != s2.charAt(i)) {
+                diff.add(i);
             }
         }
-        if(diffIndices.size() != 2) {
+
+        if(diff.size() != 2) {
             return false;
         }
 
-        int i = diffIndices.get(0), j = diffIndices.get(1);
-
-        return s1.charAt(i) == s2.charAt(j) && s1.charAt(j) == s2.charAt(i);
+        int i = diff.get(0), j = diff.get(1);
+        return (s1.charAt(i) == s2.charAt(j)) && (s1.charAt(j) == s2.charAt(i));
     }
 }
