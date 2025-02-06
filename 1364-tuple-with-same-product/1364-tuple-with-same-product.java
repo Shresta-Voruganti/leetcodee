30a5b3a2b1b47137f5a5274da0e281ad45034909
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-02-06 15:36:09 +0530

    Time: 158 ms (96.79%), Space: 70.7 MB (48.08%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.java
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.java
@@ -2,9 +2,9 @@ import java.util.Hashtable;
 class Solution {
     public int tupleSameProduct(int[] nums) {
         Hashtable<Integer, Integer> ht1 = new Hashtable<>();
-
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
                 int res = nums[i] * nums[j];
                 ht1.put(res, ht1.getOrDefault(res, 0) + 1);
             }
@@ -12,9 +12,7 @@ class Solution {
 
         int count = 0;
         for(int freq : ht1.values()) {
-            if(freq > 1) {
-                count += 4 * (freq * (freq - 1));
-            }
+            count += 4 * freq * (freq - 1);
         }
 
         return count;
