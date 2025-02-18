3b43b353b0a22bd015d9fa6e7aea78e75bad0c50
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-02-19 00:41:48 +0530

    Time: 1 ms (100%), Space: 42.7 MB (35.97%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.java
+++ 3447-clear-digits/3447-clear-digits.java
@@ -1,22 +1,14 @@
 class Solution {
     public String clearDigits(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        int i = 0;
-        while (i < sb.length()) {
-            if(i > 0 && Character.isDigit(sb.charAt(i))) {
-                sb.deleteCharAt(i);
-                int j = i - 1;
-                while(j >= 0 && j < sb.length()) {
-                    if(Character.isLetter(sb.charAt(j))) {
-                        sb.deleteCharAt(j);
-                        i--;
-                        break;
-                    }
-                    j--;
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if(Character.isDigit(c)) {
+                if(sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
                 }
             }
             else {
-                i++;
+                sb.append(c);
             }
         }
 
