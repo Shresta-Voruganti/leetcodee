e5d7ce2ad5e00e38ae5fa23559cc5bc716cd77a2
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-02-21 00:26:57 +0530

    Time: 0 ms (100%), Space: 40.4 MB (81.49%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,21 +1,22 @@
 class Solution {
     public int hammingDistance(int x, int y) {
         int val = x ^ y;
-        int sum = bitcount(val);
+        // int sum = bitcount(val);
+        int sum = Integer.bitCount(val);
 
         return sum;
     }
 
-    private int bitcount(int ele) {
-        int count = 0;
-        while(ele > 0) {
-            if((ele & 1) == 1) {
-                count++;
-            }
-            ele = ele >> 1;
-        }
+    // private int bitcount(int ele) {
+    //     int count = 0;
+    //     while(ele > 0) {
+    //         if((ele & 1) == 1) {
+    //             count++;
+    //         }
+    //         ele = ele >> 1;
+    //     }
 
-        return count;
-    }
+    //     return count;
+    // }
 
 }
