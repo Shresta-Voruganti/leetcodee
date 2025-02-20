b1d4c6907f748841f80f9712b888f15f8b20b9e7
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-02-21 00:15:37 +0530

    Time: 1 ms (99.53%), Space: 57.8 MB (5.14%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -7,7 +7,7 @@ class Solution {
 
         for(int i = 1; i < n; i++) {
             dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
-            max = Math.max(max, dp[i]);
+            max = Math.max(dp[i], max);
         }
 
         return max;
