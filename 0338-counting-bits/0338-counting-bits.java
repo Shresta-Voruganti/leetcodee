63a48fd2ac7e9ef8968df56f3a7d06d974c2c3e0
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-02-21 00:44:20 +0530

    Time: 2 ms (94.82%), Space: 46.7 MB (48.27%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,26 +1,35 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] arr = new int[n + 1];
-        for(int i = 0; i < n + 1; i++) {
-            arr[i] = i;
-        }
+        // int[] arr = new int[n + 1];
+        // for(int i = 0; i < n + 1; i++) {
+        //     arr[i] = i;
+        // }
+        // int[] res = new int[n + 1];
+        // for(int i = 0; i < n + 1; i++) {
+        //     res[i] = bitcount(arr[i]);
+        // }
+
+        // return res;
+
         int[] res = new int[n + 1];
-        for(int i = 0; i < n + 1; i++) {
-            res[i] = bitcount(arr[i]);
+        
+        for (int i = 1; i <= n; i++) {
+            // Use the relationship to calculate the number of 1s
+            res[i] = res[i >> 1] + (i & 1);
         }
 
         return res;
     }
 
-    private int bitcount(int ele) {
-        int count = 0;
-        while(ele > 0) {
-            if((ele & 1) == 1) {
-                count++;
-            }
-            ele = ele >> 1;
-        }
+    // private int bitcount(int ele) {
+    //     int count = 0;
+    //     while(ele > 0) {
+    //         if((ele & 1) == 1) {
+    //             count++;
+    //         }
+    //         ele = ele >> 1;
+    //     }
 
-        return count;
-    }
+    //     return count;
+    // }
 }
