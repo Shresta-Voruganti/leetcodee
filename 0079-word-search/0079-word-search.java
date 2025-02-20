e014de563a509e4c0c57a843c91ea2024ced9aeb
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-02-20 22:11:11 +0530

    Time: 148 ms (52.5%), Space: 41.6 MB (62.29%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,7 +5,7 @@ class Solution {
 
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
-                if(board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0)) {
+                if(board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0, rows, cols)) {
                     return true;
                 }
             }
@@ -14,19 +14,19 @@ class Solution {
         return false;
     }
 
-    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+    private boolean dfs(char[][] board, String word, int i , int j, int index, int rows, int cols) {
         if(index == word.length()) {
             return true;
         }
 
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) {
+        if(i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] != word.charAt(index)) {
             return false;
         }
 
         char temp = board[i][j];
         board[i][j] = '#';
 
-        boolean found = dfs(board, word, i + 1, j, index + 1) || dfs(board, word, i - 1, j, index + 1) || dfs(board, word, i, j + 1, index + 1) || dfs(board, word, i, j - 1, index + 1);
+        boolean found = dfs(board, word, i + 1, j, index + 1, rows, cols) || dfs(board, word, i - 1, j, index + 1, rows, cols) || dfs(board, word, i, j + 1, index + 1, rows, cols) || dfs(board, word, i, j - 1, index + 1, rows, cols);
 
         board[i][j] = temp;
 
