690942923490bbfaa0aac85aa88fd12988449adc
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-03-04 01:05:17 +0530

    Time: 4 ms (100%), Space: 68.2 MB (35.94%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,36 +1,27 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> sl = new ArrayList<>();
-        List<Integer> gl = new ArrayList<>();
-        List<Integer> pl = new ArrayList<>();
-        List<Integer> resl = new ArrayList<>();
+        int n = nums.length;
+        int[] res = new int[n];
+        int left = 0;
+        int pivotcount = 0;
+
         for(int num : nums) {
             if(num < pivot) {
-                sl.add(num);
+                res[left++] = num;
             }
-            else if(num > pivot) {
-                gl.add(num);
-            }
-            else {
-                pl.add(num);
+            else if(num == pivot) {
+                pivotcount++;
             }
         }
 
-        for(int i = 0; i < sl.size(); i++) {
-            resl.add(sl.get(i));
-        }
-        for(int j = 0; j < pl.size(); j++) {
-            resl.add(pl.get(j));
-        }
-        for(int k = 0; k < gl.size(); k++) {
-            resl.add(gl.get(k));
+        while(pivotcount-- > 0) {
+            res[left++]= pivot;
         }
 
-        int n = resl.size();
-
-        int[] res = new int[n];
-        for(int i = 0; i < n; i++) {
-            res[i] = resl.get(i);
+        for(int num : nums) {
+            if(num > pivot) {
+                res[left++] = num;
+            }
         }
 
         return res;
