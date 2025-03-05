6718e5ca0826a4b7b7158a13533d65d19b8e9568
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-03-06 01:10:29 +0530

    Time: 13 ms (4.15%), Space: 47.4 MB (5.39%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,9 +1,6 @@
 class Solution {
     public long coloredCells(int n) {
-        if(n == 0) {
-            return 1;
-        }
-        if(n == 1) {
+        if(n <= 1) {
             return 1;
         }
 
