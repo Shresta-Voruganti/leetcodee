67c942739058166a2ba4538848daa4867d7946cf
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-03-06 21:44:47 +0530

    Time: 11 ms (73.81%), Space: 45.5 MB (53.76%) - LeetHub

--- 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.java
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,11 +1,13 @@
 class Solution {
     public int minInsertions(String s) {
-        Stack<Character> stack = new Stack<>();
+        // Stack<Character> stack = new Stack<>();
+        int c = 0;
         int ans = 0;
         for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if(ch == '(') {
-                stack.push(s.charAt(i));
+                // stack.push(s.charAt(i));
+                c++;
             }
             else {
                 if(i < s.length() - 1 && s.charAt(i + 1) == ')') {
@@ -15,16 +17,20 @@ class Solution {
                     ans++;
                 }
 
-                if(stack.isEmpty()) {
+                // if(stack.isEmpty()) 
+                if(c == 0) {
                     ans++;
                 }
                 else {
-                    stack.pop();
+                    // stack.pop();
+                    c--;
                 }
             }
         }
 
-        ans += stack.size() * 2;
+        // ans += stack.size() * 2;
+        ans += c * 2;
+
         return ans;
     }
 }
