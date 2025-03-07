74c5d4ca7251dcf151467c4bf840b3459dc851f1
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-03-07 21:07:33 +0530

    Time: 90 ms (71.2%), Space: 50.2 MB (40%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,20 +1,31 @@
 class Solution {
     public int[] closestPrimes(int left, int right) {
-        int[] ans = new int[2];
+        // int[] ans = new int[2];
+        int[] ans = {-1, -1};
+
         List<Integer> res = new ArrayList<>();
-        int num1;
-        int num2;
-        while(left <= right) {
-            if(isPrime(left)) {
-                res.add(left);
+
+        boolean[] isPrime = sieve(right);
+        // while(left <= right) {
+        //     if(isPrime(left)) {
+        //         res.add(left);
+        //     }
+        //     left++;
+        // }
+
+        for(int i = Math.max(2, left); i <= right; i++) {
+            if(isPrime[i]) {
+                res.add(i);
             }
-            left++;
         }
         int n = res.size();
         if(n <= 1) {
-            Arrays.fill(ans, -1);
+            // Arrays.fill(ans, -1);
             return ans;
         }
+        
+        int num1;
+        int num2;
         int diff = Integer.MAX_VALUE;
         for(int i = 0; i < n - 1; i++) {
             num1 = res.get(i);
@@ -30,13 +41,31 @@ class Solution {
         return ans;
     }
 
-    private boolean isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
+    // private boolean isPrime(int n) {
+    //     if (n <= 1) return false;
+    //     for (int i = 2; i <= Math.sqrt(n); i++) {
+    //         if (n % i == 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    private boolean[] sieve(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        // isPrime[0] = false; isPrime[1] = false;
+        isPrime[0] = isPrime[1] = false;
+
+        for(int i = 2; i*i <= n; i++) {
+            if(isPrime[i]) {
+                for(int j = i*i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
-        return true;
+
+        return isPrime;
+        
     }
 }
