97391351016c715ca05175962b4e9c76e1735b3f
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-03-08 23:42:18 +0530

    Time: 6 ms (76.37%), Space: 58.1 MB (40.93%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
@@ -1,38 +1,25 @@
 class Solution {
     public int numOfSubarrays(int[] arr) {
-        // int MOD = 1000000007L;
         int MOD = (int) Math.pow(10, 9) + 7;
-        int n = arr.length;
-        for(int i = 0; i < n; i++) {
-            arr[i] = arr[i] & 1;
-        }
-        int[] dpEven = new int[n];
-        int[] dpOdd = new int[n];
+        int count = 0;
+        int prefixsum = 0;
+        int oddcount = 0;
+        int evencount = 1;
 
-        if(arr[n - 1] == 0) {
-            dpEven[n - 1] = 1;
-        }
-        else {
-            dpOdd[n - 1] = 1;
-        }
-
-        for(int i = n - 2; i >= 0; i--) {
-            if(arr[i] == 1) {
-                dpOdd[i] = (1 + dpEven[i + 1]) % MOD;
-                dpEven[i] = dpOdd[i + 1];
+        for(int num : arr) {
+            prefixsum += num;
+            if((prefixsum & 1) == 0) {
+                count += oddcount;
+                evencount++;
             }
             else {
-                dpEven[i] = (1 + dpEven[i + 1]) % MOD;
-                dpOdd[i] = dpOdd[i + 1];
+                count += evencount;
+                oddcount++;
             }
-        }
-
-        int count = 0;
-        for(int odd : dpOdd) {
-            count += odd;
             count %= MOD;
         }
 
+        
         return count;
     }
 }
