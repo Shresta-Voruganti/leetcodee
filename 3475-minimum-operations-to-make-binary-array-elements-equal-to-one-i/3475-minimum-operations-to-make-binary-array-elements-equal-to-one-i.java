18c5e95b8c50453fc688135626df1248cb31bb52
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-03-19 23:34:16 +0530

    Time: 8 ms (34.91%), Space: 57.3 MB (74.95%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,65 +1,27 @@
 class Solution {
     public int minOperations(int[] nums) {
-        int k = 3;
-        int count = 0;
-        boolean canflip = false;
         int n = nums.length;
-        List<Integer> res = new ArrayList<>();
+        int count = 0;
 
-        for (int i = 0; i < k; i++) {
-            res.add(nums[i]);
-        }
-
-        if (res.get(0) == 0) {
-            canflip = true;
-        }
-        if (canflip) {
-            fliplist(res);
-            updateArray(nums, 0, res); 
-            count++;
-        }
-        canflip = false;
-
-        for (int i = k; i < n; i++) {
-            res.remove(0); 
-            res.add(nums[i]); 
-
-            if (res.get(0) == 0) {
-                canflip = true;
-            }
-            if (canflip) {
-                fliplist(res);
-                updateArray(nums, i - k + 1, res);
+        for(int i = 0; i <= n - 3; i++) {
+            if(nums[i] == 0) {
+                flip(nums, i);
                 count++;
             }
-            canflip = false;
         }
 
-        if (checkones(nums)) {
-            return count;
-        }
-
-        return -1;
-    }
-
-    private void fliplist(List<Integer> res) {
-        for (int i = 0; i < res.size(); i++) {
-            res.set(i, 1 - res.get(i));
-        }
-    }
-
-    private void updateArray(int[] nums, int start, List<Integer> res) {
-        for (int i = 0; i < res.size(); i++) {
-            nums[start + i] = res.get(i);
-        }
-    }
-
-    private boolean checkones(int[] nums) {
-        for (int num : nums) {
-            if (num == 0) {
-                return false;
+        for(int num : nums) {
+            if(num == 0) {
+                return -1;
             }
         }
-        return true;
+
+        return count;
+    }
+
+    private void flip(int[] nums, int start) {
+        for(int i = start; i < start+3; i++) {
+            nums[i] = 1 - nums[i];
+        }
     }
 }
