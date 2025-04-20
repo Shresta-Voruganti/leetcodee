72cb4cf934b61035dfae0e8182d7e660972759a7
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-04-20 17:21:29 +0530

    Time: 15 ms (77.71%), Space: 44.2 MB (89.16%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+
+        for(int i = 0; i <= amount; i++) {
+            for(int coin : coins) {
+                if(i >= coin) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == max ? -1 : dp[amount];
+    }
+}
