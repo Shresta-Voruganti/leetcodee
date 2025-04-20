0c86ef7928f9e14fbb85167e27bac2ddc7604bfe
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-04-20 18:28:05 +0530

    Time: 41 ms (59.96%), Space: 44.5 MB (46.42%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,15 +1,9 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        if(nums == null || nums.length  == 0) {
-            return 0;
-        }
-
         int n = nums.length;
         int[] dp = new int[n];
-
-        for(int i = 0; i < n; i++) {
-            dp[i] = 1;
-        }
+        Arrays.fill(dp, 1);
+        dp[0] = 1;
 
         for(int i = 1; i < n; i++) {
             for(int j = 0; j < i; j++) {
@@ -18,11 +12,12 @@ class Solution {
                 }
             }
         }
-
-        int maxLen = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++) {
-            maxLen = Math.max(maxLen, dp[i]);
+        
+        int maxlen = 1;
+        for(int len : dp) {
+            maxlen = Math.max(len, maxlen);
         }
-        return maxLen;
+
+        return maxlen;
     }
 }
