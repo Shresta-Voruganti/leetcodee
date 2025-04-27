1125ec74f1bd16ab063978e35174cb39245eb48b
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-04-27 15:12:17 +0530

    Time: 1 ms (83.5%), Space: 41.2 MB (34.17%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,21 +1,21 @@
 class Solution {
     public int reverse(int x) {
-        int r = 0;
+        int revNum = 0;
 
-        while(x!=0)
-        {
-            int d = x%10;
+        // Integer.MAX_VALUE =  2147483647 → last digit 7.
+        // Integer.MIN_VALUE = -2147483648 → last digit -8.
+
+        while(x != 0) {
+            int d = x % 10;
+
+            if((revNum > Integer.MAX_VALUE/10) || (revNum < Integer.MIN_VALUE/10) || (revNum == Integer.MAX_VALUE/10 && d > 7) || (revNum == Integer.MIN_VALUE/10 && d < -8)) {
+                return 0;
+            }
+
+            revNum = (revNum * 10) + d;
             x = x/10;
-            if(r>Integer.MAX_VALUE/10 || r==Integer.MAX_VALUE/10 && d>7)
-            {
-                return 0;
-            }
-            if(r<Integer.MIN_VALUE/10 || r==Integer.MIN_VALUE/10 && d<-8)
-            {
-                return 0;
-            }
-            r = r*10 + d;
         }
-        return r;
+
+        return revNum;
     }
 }
