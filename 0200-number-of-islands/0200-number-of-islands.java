cf8eb3364884e1f2ddbb827e67c72bef67a5c33b
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-05-04 18:57:56 +0530

    Time: 4 ms (48.62%), Space: 48.5 MB (99.64%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,74 +1,42 @@
 class Solution {
-    static int[][] dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
     public int numIslands(char[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        DisjointSet dsu = new DisjointSet(row, col);
+        int m = grid.length;
+        int n = grid[0].length;
 
-        for(int i = 0; i < row * col; i++) {
-            dsu.parent[i] = i;
-            dsu.sz[i] = 1;
-        }
+        int count = 0;
 
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 if(grid[i][j] == '1') {
-                    int index = i * col + j;
-                    for(int[] dir : dirs) {
-                        int x = i + dir[0];
-                        int y = j + dir[1];
-                        if(x >= 0 && x < row && y >= 0 && y < col && grid[x][y] == '1') {
-                            int newindex = x * col + y;
-                            dsu.union(index, newindex);
-                        }
-                    }
+                    count++;
+                    bfs(i, j, grid, count, m, n);
                 }
             }
         }
 
-        Set<Integer> hs = new HashSet<>();
+        return count;
+    }
 
-        for(int i = 0; i < row * col; i++) {
-            if(grid[i / col][i % col] == '1') {
-                hs.add(dsu.findParent(i));
+    static int[][] dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    private void bfs(int i, int j, char[][] grid, int count, int m, int n) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{i, j});
+        grid[i][j] = '0';
+
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+            int x = curr[0];
+            int y = curr[1];
+
+            for(int[] dir : dirs) {
+                int newx = x + dir[0];
+                int newy = y + dir[1];
+
+                if(newx >= 0 && newx < m && newy >= 0 && newy < n && grid[newx][newy] == '1') {
+                    q.offer(new int[]{newx, newy});
+                    grid[newx][newy] = '0';
+                }
             }
-        }
-
-        return hs.size();
-    }
-}
-
-class DisjointSet {
-    int rows, cols;
-    int[] parent, sz;
-
-    public DisjointSet(int rows, int cols) {
-        this.rows = rows;
-        this.cols = cols;
-        parent = new int[rows * cols];
-        sz = new int[rows * cols];
-    }
-
-    public int findParent(int x) {
-        if(x != parent[x]) {
-            parent[x] = findParent(parent[x]);
-        }
-        return parent[x];
-    }
-
-    public void union(int x, int y) {
-        int px = findParent(x);
-        int py = findParent(y);
-        if(px == py) {
-            return;
-        }
-        if(sz[px] >= sz[py]) {
-            parent[py] = px;
-            sz[px] += py;
-        }
-        else {
-            parent[px] = py;
-            sz[py] += sz[px];
         }
     }
 }
