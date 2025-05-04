86bfedf51e53147767cd6760c40cec14a4c39ddf
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-05-04 22:01:09 +0530

    Time: 17 ms (88.4%), Space: 96.7 MB (39.87%) - LeetHub

--- 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.java
+++ 1663-detect-cycles-in-2d-grid/1663-detect-cycles-in-2d-grid.java
@@ -1,77 +1,43 @@
 class Solution {
-    static int[][] dirs = {{0, -1}, {-1, 0}};
+    static int[][] dirs = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
     public boolean containsCycle(char[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
 
-        DisjointSet dsu = new DisjointSet(row * col);
+        boolean[][] vis = new boolean[row][col];
 
         for(int i = 0; i < row; i++) {
             for(int j = 0; j < col; j++) {
-                int index = i * col + j;
-
-                for(int[] dir : dirs) {
-                    int x = i + dir[0];
-                    int y = j + dir[1];
-
-                    if(x >= 0 && x < row && y >= 0 && y < col && grid[i][j] == grid[x][y]) {
-                        int newindex = x * col + y;
-                        if (dsu.findParent(index) == dsu.findParent(newindex)) {
-                            return true;
-                        }
-
-                        dsu.union(index, newindex);
+                if (!vis[i][j]) {
+                    if (dfs(grid, i, j, -1, -1, vis, grid[i][j])) {
+                        return true;
                     }
                 }
             }
         }
         return false;
     }
-}
 
-class DisjointSet {
-    int n;
-    int[] parent, rank;
+    public boolean dfs(char[][] grid, int x, int y, int prevX, int prevY, boolean[][] vis, char val) {
+        vis[x][y] = true;
 
-    public DisjointSet(int n) {
-        this.n = n;
-        parent = new int[n];
-        rank = new int[n];
-        makeset();
-    }
+        for(int[] dir : dirs) {
+            int newX = x + dir[0];
+            int newY = y + dir[1];
 
-    public void makeset() {
-        for(int i = 0; i < n; i++) {
-            parent[i] = i;
-            rank[i] = 0;
+            if(newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length) {
+                if(grid[newX][newY] == val) {
+                    if(!vis[newX][newY]) {
+                        if(dfs(grid, newX, newY, x, y, vis, val)) {
+                            return true;
+                        }
+                    }
+                    else if((newX != prevX) || (newY != prevY)) {
+                        return true;
+                    }
+                }
+            } 
         }
-    }
-
-    public int findParent(int x) {
-        if(x != parent[x]) {
-            parent[x] = findParent(parent[x]);
-        }
-        return parent[x];
-    }
-
-    public boolean union(int x, int y) {
-        int px = findParent(x);
-        int py = findParent(y);
-
-        if(px == py) {
-            return true;
-        }
-
-        if(rank[px] > rank[py]) {
-            parent[py] = px;
-        }
-        else if(rank[px] < rank[py]) {
-            parent[px] = py;
-        }
-        else {
-            parent[py] = px;
-            rank[px] = rank[px] + 1;
-        }
-        return false;
+    return false;
     }
 }
