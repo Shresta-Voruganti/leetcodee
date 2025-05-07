66e15447958ce5943fa83ad706222badc39cf05d
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-05-07 19:37:46 +0530

    Time: 4 ms (92.14%), Space: 45.4 MB (95.82%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,43 +1,41 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<Integer>[] graph = new ArrayList[numCourses];
-        int[] inDegree = new int[numCourses];
-        List<Integer> result = new ArrayList<>();
+        List<Integer>[] adj = new ArrayList[numCourses];
+        int[] indegree = new int[numCourses];
 
         for(int i = 0; i < numCourses; i++) {
-            graph[i] = new ArrayList<>();
+            adj[i] = new ArrayList<>();
         }
 
-        for(int[] dep : prerequisites) {
-            int course = dep[0];
-            int prereq = dep[1];
-            graph[prereq].add(course);
-            inDegree[course]++;
+        for(int[] pre : prerequisites) {
+            int u = pre[0];
+            int v = pre[1];
+            adj[v].add(u);
+            indegree[u]++;
         }
 
         Queue<Integer> q = new LinkedList<>();
+        int[] topo = new int[numCourses];
+
         for(int i = 0; i < numCourses; i++) {
-            if(inDegree[i] == 0) {
+            if(indegree[i] == 0) {
                 q.add(i);
             }
         }
 
-        while(!q.isEmpty()) {
-            int course = q.poll();
-            result.add(course);
+        int index = 0;
+        while(!q.isEmpty()) 
+        {
+            int node = q.poll();
+            topo[index++] = node;
 
-            for(int nextCourse : graph[course]) {
-                inDegree[nextCourse]--;
-                if(inDegree[nextCourse] == 0) {
-                    q.add(nextCourse);
+            for(int neig : adj[node]) {
+                indegree[neig]--;
+                if(indegree[neig] == 0) {
+                    q.add(neig);
                 }
             }
         }
-
-        if(result.size() != numCourses) {
-            return new int[0];
-        }
-
-        return result.stream().mapToInt(i -> i).toArray();
+        return index == numCourses ? topo : new int[0];
     }
 }
