311b5755815b44e9b8bec76d5758b3287603c21b
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-05-13 23:00:12 +0530

    Time: 2 ms (72.4%), Space: 44.5 MB (78.38%) - LeetHub

---
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        DisjointSet dsu = new DisjointSet(n);
+        int counter = n;
+        for(int[] edge : edges) {
+            int x = edge[0];
+            int y = edge[1];
+            int fx = dsu.findParent(x);
+            int fy = dsu.findParent(y);
+            if(fx != fy) {
+                counter--;
+                dsu.union(fx, fy);
+            }
+        }
+        return counter;
+    }
+}
+
+class DisjointSet {
+    int n;
+    int[] parent, rank;
+
+    public DisjointSet(int n) {
+        this.n = n;
+        parent = new int[n];
+        rank = new int[n];
+        makeset();
+    }
+
+    public void makeset() {
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int findParent(int x) {
+        if(x != parent[x]) {
+            parent[x] = findParent(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int px = findParent(x);
+        int py = findParent(y);
+
+        if(px == py) {
+            return;
+        }
+        if(rank[px] > rank[py]) {
+            parent[py] = px;
+//            parent[py] = parent[px];
+        }
+        else if(rank[px] < rank[py]) {
+            parent[px] = py;
+//            parent[px] = parent[py];
+        }
+        else {
+            parent[py] = px;
+//            parent[py] = parent[px];
+            rank[px] = rank[px] + 1;
+        }
+    }
+}
