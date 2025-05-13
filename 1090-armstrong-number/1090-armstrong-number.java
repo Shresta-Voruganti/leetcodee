242d9f03ba5ecc8e30b02265bf2f520923c85422
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-05-13 22:51:27 +0530

    Time: 0 ms (100%), Space: 40.3 MB (73.86%) - LeetHub

---
+++ 1090-armstrong-number/1090-armstrong-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isArmstrong(int n) {
+        int sum = 0;
+        int l = String.valueOf(n).length();
+        int t = n;
+        while(n>0)
+        {
+            sum+= Math.pow(n%10,l);
+            n = n/10;
+        }
+        if(sum == t)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
