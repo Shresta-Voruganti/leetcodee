dd49a00d45b59eed6ea7f3615638be61116c9b0f
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-05-14 02:27:03 +0530

    Time: 0 ms (100%), Space: 40.8 MB (26.67%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for(int[] d : dp) {
+            Arrays.fill(d, 1);
+        }
+
+        for(int row = 1; row < m; row++) {
+            for(int col = 1; col < n; col++) {
+                dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
