62bcdd39133d8be53cba04937110b9e984f9b66f
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-05-17 15:11:10 +0530

    Time: 6 ms (97.67%), Space: 59.5 MB (79.46%) - LeetHub

--- 0361-bomb-enemy/0361-bomb-enemy.java
+++ 0361-bomb-enemy/0361-bomb-enemy.java
@@ -2,47 +2,34 @@ class Solution {
     public int maxKilledEnemies(char[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        int[][] dp = new int[rows][cols];
+        int rowHits = 0;
+        int[] colHits = new int[cols];
         int max = 0;
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
+                if(j == 0 || grid[i][j - 1] == 'W') {
+                    rowHits = 0;
+                    for(int k = j; k < cols && grid[i][k] != 'W'; k++) {
+                        if(grid[i][k] == 'E') {
+                            rowHits++;
+                        }
+                    }
+                }
+                if(i == 0 || grid[i - 1][j] == 'W') {
+                    colHits[j] = 0;
+                    for(int k = i; k < rows && grid[k][j] != 'W'; k++) {
+                        if(grid[k][j] == 'E') {
+                            colHits[j]++;
+                        }
+                    }
+                }
+
                 if(grid[i][j] == '0') {
-                    int val = traverse(grid, i, j, rows, cols);
-                    max = Math.max(max, val);
+                    max = Math.max(max, rowHits + colHits[j]);
                 }
             }
         }
 
         return max;
-    }
-
-    private int traverse(char[][] grid, int i, int j, int rows, int cols) {
-        int kills = 0;
-
-        for(int x = j - 1; x >= 0 && grid[i][x] != 'W'; x--) {
-            if(grid[i][x] == 'E') {
-                kills++;
-            }
-        }
-
-        for(int x = j + 1; x < cols && grid[i][x] != 'W'; x++) {
-            if(grid[i][x] == 'E') {
-                kills++;
-            }
-        }
-
-        for(int y = i - 1; y >= 0 && grid[y][j] != 'W'; y--) {
-            if(grid[y][j] == 'E') {
-                kills++;
-            }
-        }
-
-        for(int y = i + 1; y < rows && grid[y][j] != 'W'; y++) {
-            if(grid[y][j] == 'E') {
-                kills++;
-            }
-        }
-
-        return kills;
     }
 }
