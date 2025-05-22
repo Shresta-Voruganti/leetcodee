f626b353082431508191881493870b0334d8863b
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-05-22 17:40:46 +0530

    Time: 31 ms (24.63%), Space: 97.7 MB (95.89%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
@@ -2,10 +2,13 @@ class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
         int left = 0, right = queries.length;
         int ans = -1;
+        int n = nums.length;
+        long[] dec = new long[n + 2]; // reused difference array
+        // difference array (use long to avoid overflow)
 
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (canZero(nums, queries, mid)) {
+            if (canZero(nums, queries, mid, dec)) {
                 ans = mid;
                 right = mid - 1; // try fewer queries
             } else {
@@ -16,9 +19,14 @@ class Solution {
         return ans;
     }
 
-    private boolean canZero(int[] nums, int[][] queries, int k) {
+    private boolean canZero(int[] nums, int[][] queries, int k, long[] dec)
+    {
         int n = nums.length;
-        long[] dec = new long[n + 2]; // difference array (use long to avoid overflow)
+
+        // Clear the dec array only from 0 to n + 1
+        for (int i = 0; i <= n + 1; i++) {
+            dec[i] = 0;
+        }
 
         for (int i = 0; i < k; i++) {
             int l = queries[i][0];
