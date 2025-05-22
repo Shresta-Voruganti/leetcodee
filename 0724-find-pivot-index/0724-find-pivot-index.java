eeec816049aeb7a67dba25523824007a35448a54
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-05-22 10:57:46 +0530

    Time: 1 ms (92.73%), Space: 45.6 MB (54.86%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,44 +1,20 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         int n = nums.length;
+        int totalSum = 0;
+        for(int num : nums) {
+            totalSum += num;
+        }
         int bal = -1;
-
+        int leftSum = 0;
         for(int i = 0; i < n; i++) {
-            int sumg = 0;
-            int sumd = 0;
-            if(i == 0) {
-                sumg  += sumCalc(nums, 0, 1);
-                sumd = 0;
-            }
-            else if(i == n - 1) {
-                sumg = 0;
-                sumd += sumCalc(nums, n - 1, -1);
-            }
-            else {
-                sumg += sumCalc(nums, i, 1);
-                sumd += sumCalc(nums, i, -1);
-            }
-            if(sumg == sumd) {
+            int rightSum = totalSum - leftSum - nums[i];
+            if(leftSum == rightSum) {
                 bal = i;
                 break;
             }
+            leftSum += nums[i];
         }
         return bal;
-    }
-
-    private int sumCalc(int[] arr, int index, int inc) {
-        int n = arr.length;
-        int sum = 0;
-        if(inc > 0) {
-            for(int i = index + 1; i < n; i++) {
-                sum += arr[i];
-            }
-        }
-        else {
-            for(int i = index - 1; i >= 0; i--) {
-                sum += arr[i];
-            }
-        }
-        return sum;
     }
 }
