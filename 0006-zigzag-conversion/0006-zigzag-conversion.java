9235d0814797566c6c7db6e9c7339285f3ddf950
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-05-22 12:24:33 +0530

    Time: 4 ms (82.2%), Space: 45.1 MB (56.46%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+
+        int n = s.length();
+
+        StringBuilder[] arr = new StringBuilder[numRows];
+        for (int j = 0; j < numRows; j++) {
+            arr[j] = new StringBuilder();
+        }
+
+        int i = 0;
+        int count = 0;
+        while(i < n) {
+            while(count < numRows - 1 && i < n) {
+                arr[count].append(s.charAt(i));
+                count++;
+                i++;
+            }
+            while(count > 0 && i < n) {
+                arr[count].append(s.charAt(i));
+                count--;
+                i++;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int j = 0; j < numRows; j++) {
+            sb.append(arr[j]);
+        }
+        return sb.toString();
+    }
+}
