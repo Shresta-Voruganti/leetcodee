599b4e0accadef6d750d067d130c14a612820438
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-06-03 20:32:40 +0530

    Time: 5 ms (8.01%), Space: 43 MB (35.56%) - LeetHub

---
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Map<Integer, List<Pair>> hmap = new TreeMap<>();
+        traverse(0, root, hmap, 0);
+        for(List<Pair> l : hmap.values()) {
+            l.sort(Comparator.comparingInt(p -> p.level));
+            List<Integer> temp = new ArrayList<>();
+            for(Pair p : l) {
+                temp.add(p.value);
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+
+    private void traverse(int sc, TreeNode root, Map<Integer, List<Pair>> hmap, int level) {
+        if(root == null) {
+            return;
+        }
+        if(!hmap.containsKey(sc)) {
+            hmap.put(sc, new ArrayList<>());
+        }
+        hmap.get(sc).add(new Pair(level, root.val));
+        traverse(sc - 1, root.left, hmap, level + 1);
+        traverse(sc + 1, root.right, hmap, level + 1);
+    }
+
+    class Pair {
+        int level;
+        int value;
+        Pair(int level, int value) {
+            this.level = level;
+            this.value = value;
+        }
+    }
+}
