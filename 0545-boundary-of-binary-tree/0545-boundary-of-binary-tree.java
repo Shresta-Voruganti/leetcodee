b9e52b1f0fb81be87fa193d9b0a7b203529ccc70
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-06-03 22:11:44 +0530

    Time: 1 ms (77.46%), Space: 44.6 MB (59.22%) - LeetHub

---
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> nodes = new ArrayList<>(1000);
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        if(root == null) {
+            return nodes;
+        }
+        nodes.add(root.val);
+        if(root.left != null) {
+            leftBoundary(root.left);
+        }
+        // leftBoundary(root.left);
+        leaves(root.left);
+        leaves(root.right);
+        if(root.right != null) {
+            rightBoundary(root.right);
+        }
+        // rightBoundary(root.right);
+        return nodes;
+    }
+
+    private void leftBoundary(TreeNode root) {
+        if(root == null || ((root.left == null) && (root.right == null))) {
+            return;
+        }
+        nodes.add(root.val);
+        if(root.left == null) {
+            leftBoundary(root.right);
+        }
+        else {
+            leftBoundary(root.left);
+        }
+    }
+
+    private void rightBoundary(TreeNode root) {
+        if(root == null || ((root.left == null) && (root.right == null))) {
+            return;
+        }
+        if(root.right == null) {
+            rightBoundary(root.left);
+        }
+        else {
+            rightBoundary(root.right);
+        }
+        nodes.add(root.val);
+    }
+
+    private void leaves(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        if((root.left == null) && (root.right == null)) {
+            nodes.add(root.val);
+            return;
+        }
+        leaves(root.left);
+        leaves(root.right);
+    }
+}
