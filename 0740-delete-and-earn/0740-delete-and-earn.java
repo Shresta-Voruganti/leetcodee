e06eaf5b2ab39e7c40e8e8078a6030c8a15b1cf5
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-06-10 18:37:37 +0530

    Time: 12 ms (33.56%), Space: 44.6 MB (65.05%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int maxele = 0;
+        int sum = 0;
+        Map<Integer, Integer> hmap = new HashMap<>();
+        for(int le : nums) {
+            maxele = Math.max(maxele, le);
+            sum += le;
+            hmap.put(le, hmap.getOrDefault(le, 0) + 1);
+        }
+        int[] dp = new int[maxele + 1];
+        dp[1] = 1 * hmap.getOrDefault(1, 0);
+
+        for(int i = 2; i < maxele + 1; i++) {
+            int isum = i * hmap.getOrDefault(i, 0);
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + isum);
+        }
+
+        return dp[maxele];
+    }
+}
