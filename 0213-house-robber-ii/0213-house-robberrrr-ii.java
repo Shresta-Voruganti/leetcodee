cfe7446cef137c77d0cb4d4d82dc646a8f09bcec
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-06-10 22:06:36 +0530

    Time: 3 ms (1.91%), Space: 41.5 MB (18.91%) - LeetHub

--- 0213-house-robber-ii/0213-house-robberrrr-ii.java
+++ 0213-house-robber-ii/0213-house-robberrrr-ii.java
@@ -4,39 +4,41 @@ class Solution {
         if(n == 1) {
             return nums[0];
         }
+        // int[] temp1 = new int[n - 1];
         List<Integer> temp1 = new ArrayList<>();
+        // int[] temp2 = new int[n - 1];
         List<Integer> temp2 = new ArrayList<>();
+
         for(int i = 0; i < n; i++) {
             if(i != 0) {
                 temp1.add(nums[i]);
             }
-            if(i != (n - 1)) {
+            if(i != n - 1) {
                 temp2.add(nums[i]);
             }
         }
 
-        int max = Math.max(rob1(temp1.stream().mapToInt(i -> i).toArray()), rob1(temp2.stream().mapToInt(i -> i).toArray()));
-        return max;
+        int curi = Math.max(rob1(temp1.stream().mapToInt(i -> i).toArray()), rob1(temp2.stream().mapToInt(i -> i).toArray()));
+
+        return curi;
     }
 
-    private int rob1(int[] arr) {
-        int n = arr.length;
-        if(n == 0) {
-            return 0;
-        }
-        if(n == 1) {
-            return arr[0];
-        }
-        int[] dp = new int[n];
-        
-        dp[0] = arr[0];
-        dp[1] = Math.max(arr[0], arr[1]);
+    private int rob1(int[] nums) {
+        int n = nums.length;
+        int prev = nums[0];
+        int prev2 = 0;
 
-        for(int i = 2; i < n; i++) {
-            int take = arr[i] + dp[i - 2];
-            int not_take = dp[i - 1];
-            dp[i] = Math.max(take, not_take);
+        for(int i = 1; i < n; i++) {
+            int take = nums[i];
+            if(i > 1) {
+                take += prev2;
+            }
+            int not_take = 0 + prev;
+            int curi = Math.max(take, not_take);
+            prev2 = prev;
+            prev = curi;
         }
-        return dp[n - 1];
+
+        return prev;
     }
 }
