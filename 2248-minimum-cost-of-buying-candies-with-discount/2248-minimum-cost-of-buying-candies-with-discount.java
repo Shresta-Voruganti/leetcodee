5b8c40089f11542a77e9990bb3889f32801a4139
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-07-22 20:10:23 +0530

    Time: 8 ms (4.86%), Space: 43.2 MB (6.23%) - LeetHub

--- 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
@@ -10,7 +10,7 @@ class Solution {
         int a = 2;
         int d = 3;
         for(int i = 0; i < n; i++) {
-            if((i - a)%d == 0 && i >= a) {
+            if((i - a) % d == 0 && i >= a) {
                 continue;
             }
             sum += costs.get(i);
