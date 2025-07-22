05e21c26ecb9cb7d83a542ed83e65f40254da079
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-07-22 20:47:51 +0530

    Time: 5 ms (66.66%), Space: 44.7 MB (5.07%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,27 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0)
-        {
+        if(x < 0) {
             return false;
         }
-        String s = String.valueOf(x);
-        char[] chh = s.toCharArray();
-        int[] num = new int[chh.length];
-        int l = num.length;
-        for(int i=0; i<l; i++)
-        {
-            num[i] = chh[i] - '0';
-        }
-        
-        for(int i=0; i<l; i++)
-        {
-            int t = l-1-i;
-            if(num[i]!=num[t])
-            {
-                return false;
-            }
+        int n = x;
+        int sum = 0;
+        while(x > 0) {
+            int d = x % 10;
+            sum = sum * 10 + d;
+            x = x / 10;
         }
 
-        return true;
+        return (sum == n);
     }
 }
