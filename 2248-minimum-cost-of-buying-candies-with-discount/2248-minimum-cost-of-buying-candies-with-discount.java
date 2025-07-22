c80f87fe92e51fa5445a7c31b43edefffc6c20b4
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-07-22 20:12:00 +0530

    Time: 8 ms (4.86%), Space: 42.5 MB (39.23%) - LeetHub

--- 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
@@ -7,6 +7,13 @@ class Solution {
             return costs.get(0) + costs.get(1);
         }
         int sum = 0;
+        /*
+| Concept             | Your formula      | Java condition        |
+| ------------------- | ----------------- | --------------------- |
+| AP indices          | t_n = a + (n-1)d  | `i = a + (n - 1) * d` |
+| Rearranged for i    | (i - a) % d == 0  | `(i - a) % d == 0`    |
+| Apply only if valid | i >= a            | `i >= a`              |
+        */
         int a = 2;
         int d = 3;
         for(int i = 0; i < n; i++) {
