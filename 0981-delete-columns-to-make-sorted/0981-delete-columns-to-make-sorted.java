7dcfda9b7dc779a4380b183c359e1f36fb33a25e
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-07-22 19:38:45 +0530

    Time: 8 ms (65.7%), Space: 44.6 MB (35.27%) - LeetHub

--- 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.java
+++ 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.java
@@ -3,7 +3,9 @@ class Solution {
         int m = strs.length;
         int n = strs[0].length();
         int countdel = 0;
+        //each col
         for(int i = 0; i < n; i++) {
+            //each row
             for(int j = 0; j < m - 1; j++) {
                 boolean check = strs[j].charAt(i) <= strs[j + 1].charAt(i);
                 if(check == false) {
