0df7de401b42000e6a1aca616a3d2b9488f76332
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-04 19:21:03 +0530

    Time: 2 ms (83.03%), Space: 45.6 MB (71.23%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,18 +2,20 @@ class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
         if(nums[0] >= n - 1) return true;
-        boolean[] dp = new boolean[n];
-        dp[0] = true;
-        for(int i = 0; i < n; i++) {
-            if(!dp[i]) continue;
+        int maxreach = 0;
 
-            for(int j = 1; j <= nums[i]; j++) {
-                if(i + j < n) {
-                    dp[i + j] = true;
-                }
+        for(int i = 0; i < n; i++) {
+            if(i > maxreach) {
+                return false;
+            }
+
+            maxreach = Math.max(maxreach, i + nums[i]);
+
+            if(maxreach >= n - 1) {
+                return true;
             }
         }
-        
-        return dp[n - 1];
+
+        return false;
     }
 }
