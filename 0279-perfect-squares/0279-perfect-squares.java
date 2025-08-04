94b14ca11ef3b07c9c75a036e094862b111a0f4f
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-04 18:40:22 +0530

    Time: 33 ms (67.73%), Space: 42.5 MB (87.88%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -3,12 +3,14 @@ class Solution {
         if(isPerfectSquare(n)) return 1;
 
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        // Arrays.fill(dp, Integer.MAX_VALUE);
+        Arrays.fill(dp, n + 1);
         dp[0] = 0;
 
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                int square = j * j;
+                dp[i] = Math.min(dp[i], dp[i - square] + 1);
             }
         }
 
