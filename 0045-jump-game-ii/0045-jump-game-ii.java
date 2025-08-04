6d4f898a1ccfdac374b5e6ca07f231ef7a369434
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-04 19:39:36 +0530

    Time: 1 ms (99.51%), Space: 44.8 MB (94.01%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        // if(nums[0] >= n - 1) return 1;
+        int steps = 0;
+        int curend = 0;
+        int maxreach = 0;
+
+        // loop through all positions except the last one - as we don't need the value at last index
+        for(int i = 0; i < n - 1; i++) {
+            maxreach = Math.max(maxreach, i + nums[i]);
+
+            if(i == curend) {
+                steps++;
+                curend = maxreach;
+            }
+        }
+
+        return steps;
+    }
+}
