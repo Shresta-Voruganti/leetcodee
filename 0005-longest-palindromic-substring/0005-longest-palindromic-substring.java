1d326de43ea6334f79cdb1ad4a041e0520c30275
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-04 16:58:38 +0530

    Time: 2547 ms (5.08%), Space: 45.3 MB (38.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,35 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if(s == null || s.length() == 0) return "";
-
-        int start = 0;
-        int end = 0;
-
+        if(s == null || s.length() == 0) return new String("");
+        if(s.length() == 1) return s;
+        int maxlen = 0;
+        String res = "";
         for(int i = 0; i < s.length(); i++) {
-            int len1 = getPalLength(s, i, i);
-            int len2 = getPalLength(s, i, i + 1);
-            int len = Math.max(len1, len2);
-
-            if(len > end - start) {
-                start = i - (len - 1)/2;
-                end = i + len/2;
+            for(int j = i + 1; j <= s.length(); j++) {
+                String sub = s.substring(i, j);
+                if(checkpal(sub)) {
+                    int len = sub.length();
+                    if(len > maxlen) {
+                        maxlen = len;
+                        res = sub;
+                    } 
+                }
             }
         }
-
-        return s.substring(start, end + 1);
+        if(res.equals("")) return String.valueOf(s.charAt(0));
+        return res;
     }
 
-    private int getPalLength(String s, int left, int right) {
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    private boolean checkpal(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right) {
+            if(s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
         }
-
-        return right - left - 1;
+        return true;
     }
 }
