89a5f4ae7d44db2fa6b310dcbd89f51bee971d73
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-06 01:39:22 +0530

    Time: 0 ms (100%), Space: 44.4 MB (36.36%) - LeetHub

---
+++ 0828-chalkboard-xor-game/0828-chalkboard-xor-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean xorGame(int[] nums) {
+        int xor = 0;
+        for(int num : nums) {
+            xor ^= num;
+        }
+
+        if(xor == 0) {
+            return true;
+        }
+
+        return nums.length % 2 == 0;
+    }
+}
