7b77cdbc9c10ed25b685a761c6601fd83950d1d1
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-06 03:31:59 +0530

    Time: 13 ms (33.53%), Space: 60.3 MB (12.91%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,19 +1,13 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Map<Integer, Integer> hmap = new TreeMap<>();
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] > 0) {
-                hmap.put(nums[i], hmap.getOrDefault(nums[i], 0) + 1);
-            }
+        Set<Integer> hset = new HashSet<>();
+        for(int num : nums) {
+            hset.add(num);
         }
+
         int n = 1;
-        for(int key : hmap.keySet()) {
-            if(n != key) {
-                return n;
-            }
-            else {
-                n++;
-            }
+        while(hset.contains(n)) {
+            n++;
         }
         return n;
     }
