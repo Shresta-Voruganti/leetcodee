6ac566ad34a8f98e9a072fb7b5a0d959a605325c
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-22 03:07:00 +0530

    Time: 9 ms (96.21%), Space: 57 MB (38.33%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -30,21 +30,8 @@ class Solution {
     }
 
     private ListNode merge(ListNode l1, ListNode l2) {
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
-
-        ListNode head = null;
-        ListNode tail = null;
-
-        if(l1.val < l2.val) {
-            head = l1;
-            l1 = l1.next;
-        }
-        else {
-            head = l2;
-            l2 = l2.next;
-        }
-        tail = head;
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
 
         while(l1 != null && l2 != null) {
             if(l1.val < l2.val) {
@@ -65,6 +52,6 @@ class Solution {
             tail.next = l2;
         }
 
-        return head;
+        return dummy.next;
     }
 }
