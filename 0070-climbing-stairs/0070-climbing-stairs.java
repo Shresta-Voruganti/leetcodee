9fea3c19c97fe5c1808f5baf6015d9318066699e
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-21 23:52:53 +0530

    Time: 0 ms (100%), Space: 40.7 MB (27.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,16 @@
 class Solution {
+    Map<Integer, Integer> memo = new HashMap<>();
     public int climbStairs(int n) {
-        if(n == 1) return 1;
-        
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        
-        for(int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp(n);
+    }
+
+    private int dp(int i) {
+        if(i <= 2) return i;
+
+        if(!memo.containsKey(i)) {
+            memo.put(i, dp(i - 1) + dp(i - 2));
         }
 
-        return dp[n];
+        return memo.get(i);
     }
 }
