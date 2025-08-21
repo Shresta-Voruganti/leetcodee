fa702461f49b7e4dbbc8dcced9efc74945a9ca58
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-22 00:40:59 +0530

    Time: 0 ms (100%), Space: 44.9 MB (18.1%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,17 +11,19 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode p1 = head;
-        Set<ListNode> vis = new HashSet<>();
-        while(p1 != null) {
-            if(!vis.contains(p1)) {
-                vis.add(p1);
-                p1 = p1.next;
-            }
-            else {
+        if(head == null || head.next == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
                 return true;
             }
         }
+
         return false;
     }
 }
