60c299cfb1659e5093d3f9b1d22e6a42a1fee1fe
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-29 21:32:42 +0530

    Time: 7 ms (64.5%), Space: 47.8 MB (83.22%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,45 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // Set<char[]> checks = new HashSet();
-        // Map<char[], List<String>> hmap = new HashMap<>();
-        Set<String> checks = new HashSet<>();
-        Map<String, List<String>> hmap = new HashMap<>();
         List<List<String>> res = new ArrayList<>();
-
-        for(String s : strs) {
-            char[] chs = s.toCharArray();
-            Arrays.sort(chs);
-            // checks.add(chs);
-            checks.add(new String(chs));
-        }
-        
-        // for(char[] ch : checks) {
-        //     hmap.put(ch, new ArrayList<>());
-        // }
-
-        for (String key : checks) {
-            hmap.put(key, new ArrayList<>());
-        }
+        Map<String, List<String>> hmap = new HashMap<>();
 
         for(String s : strs) {
             char[] chs = s.toCharArray();
             Arrays.sort(chs);
             String key = new String(chs);
-            // List<String> ll = hmap.get(chs);
-            List<String> ll = hmap.get(key);
-            ll.add(s);
-            // hmap.put(chs, ll);       
-            hmap.put(key, ll);     
+            hmap.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
 
-        // for(Map.Entry<char[], List<String>> e : hmap.entrySet()) {
-        //     List<String> ll = e.getValue();
-        //     res.add(ll);
-        // }
-
-        for (Map.Entry<String, List<String>> e : hmap.entrySet()) {
-            res.add(e.getValue());
-        }
+        res.addAll(hmap.values());
 
         return res;
     }
