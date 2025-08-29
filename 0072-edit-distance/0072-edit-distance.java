9d2ff91f6a312d60b4aff914c3891703b539cb38
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-29 15:45:39 +0530

    Time: 5 ms (60.32%), Space: 45.4 MB (18.4%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,31 +2,27 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-        int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = i;
         }
-        return f(m - 1, n - 1, word1.toCharArray(), word2.toCharArray(), dp);
-    }
+        for(int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
 
-    private int f(int i, int j, char[] s1, char[] s2, int[][] dp) {
-        if(i < 0) return j + 1;
-        if(j < 0) return i + 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i] == s2[j]) return dp[i][j] = 0 + f(i - 1, j - 1, s1, s2, dp);
-        else {
-            return dp[i][j] = Math.min(
-                1 + f(i - 1, j - 1, s1, s2, dp), Math.min(
-                    1 + f(i, j - 1, s1, s2, dp),
-                    1 + f(i - 1, j, s1, s2, dp)
-                )
-            );
-            // return dp[i][j] = 1 + Math.min(
-            //     f(i - 1, j - 1, s1, s2, dp), Math.min(
-            //         f(i, j - 1, s1, s2, dp),
-            //         f(i - 1, j, s1, s2, dp)
-            //     )
-            // );
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = 0 + dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(
+                        dp[i - 1][j], dp[i][j - 1]
+                    ));
+                }
+            }
         }
+
+        return dp[m][n];
     }
 }
