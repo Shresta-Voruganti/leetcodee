7acf14f5a23b3c88ad405299324f737ccf8c24e2
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-29 15:50:24 +0530

    Time: 3 ms (94.45%), Space: 44.9 MB (78.7%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,18 +2,18 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-        int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++) {
+        int[][] dp = new int[m + 1][n + 1];
+        for(int i = 0; i <= m; i++) {
             Arrays.fill(dp[i], -1);
         }
-        return f(m - 1, n - 1, word1.toCharArray(), word2.toCharArray(), dp);
+        return f(m, n, word1.toCharArray(), word2.toCharArray(), dp);
     }
 
     private int f(int i, int j, char[] s1, char[] s2, int[][] dp) {
-        if(i < 0) return j + 1;
-        if(j < 0) return i + 1;
+        if(i == 0) return j;
+        if(j == 0) return i;
         if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i] == s2[j]) return dp[i][j] = 0 + f(i - 1, j - 1, s1, s2, dp);
+        if(s1[i - 1] == s2[j - 1]) return dp[i][j] = 0 + f(i - 1, j - 1, s1, s2, dp);
         else {
             return dp[i][j] = Math.min(
                 1 + f(i - 1, j - 1, s1, s2, dp), Math.min(
