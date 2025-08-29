66268f5d3d9da971592cf254aaaf33738b6f8f84
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-08-30 05:29:22 +0530

    Time: 4 ms (79.11%), Space: 44.8 MB (28.7%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -11,13 +11,12 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
 
         for(ListNode node : lists) {
             if(node != null) pq.add(node);
         }
-
-        ListNode dummy = new ListNode(-1);
-        ListNode tail = dummy;
 
         while(!pq.isEmpty()) {
             ListNode minNode = pq.poll();
