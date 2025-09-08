8de60d3f29877050d9cb71ededea565bb53b5723
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-09-08 14:50:48 +0530

    Time: 75 ms (45.91%), Space: 45.2 MB (74.51%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String t = s + s;
+        if(t.substring(1, t.length() - 1).contains(s)) {
+            return true;
+        }
+        return false;
+    }
+}
