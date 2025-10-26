850d5f512aa904f8b6197231cfe0357988dbefef
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-10-26 13:57:47 +0530

    Time: 2 ms (16.91%), Space: 40.9 MB (15.32%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -1,22 +1,13 @@
 class Solution {
     public int totalMoney(int n) {
-        int[] arr = new int[n + 1];
-        arr[0] = 0;
-        arr[1] = 1;
-        for(int i = 8; i <= n; i+=7) {
-            arr[i] = arr[i - 7] + 1;
-        }
-        for(int i = 1; i <= n; i+=7) {
-            for(int j = i + 1; j < i + 7 && j <= n; j++) {
-                arr[j] = arr[j - 1] + 1;
-            }
-        }
-        
-        int sum = 0;
+        int total = 0;
+        int start = 1;
+
         for(int i = 1; i <= n; i++) {
-            sum += arr[i];
+            total += start + (i - 1) % 7;
+            if(i % 7 == 0) start++;
         }
 
-        return sum;
+        return total;
     }
 }
