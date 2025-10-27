a276f7314397b56860a921564bc25d0258be7a91
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-10-27 15:39:17 +0530

    Time: 2 ms (78.99%), Space: 42.1 MB (93.27%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
@@ -1,35 +1,20 @@
 class Solution {
     public boolean hasSameDigits(String s) {
         int n = s.length();
-        if(n < 3) {
-            return false;
-        }        
-        String res = calc(s);
-
-        while(res.length() > 2) {
-            res = calc(res);
-        }
-
-        if(res.length() == 2) {
-            if((res.charAt(0) - '0') == (res.charAt(1) - '0')) {
-                return true;
-            }
-        }
-
-        return false;
-
-    }
-
-    private String calc(String s) {
-        int n = s.length();
-        StringBuilder sb = new StringBuilder();
+        if(n < 3) return false;
+        int[] arr = new int[n];
         for(int i = 0; i < n; i++) {
-            if(i + 1 >= n) {
-                continue;
-            }
-            int sum = (s.charAt(i) - '0') + (s.charAt(i + 1) - '0'); 
-            sb.append(String.valueOf(sum % 10));
+            arr[i] = s.charAt(i) - '0';
         }
-        return sb.toString();
+
+        while(n > 2) {
+            for(int i = 0; i < n - 1; i++) {
+                arr[i] = (arr[i] + arr[i + 1]) % 10;
+            }
+            n--;
+        }
+
+        return arr[0] == arr[1];
+
     }
 }
