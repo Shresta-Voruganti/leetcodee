d236563a874ef326613616155c59d0f33fafa44f
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2025-10-29 15:53:21 +0530

    Time: 0 ms (100%), Space: 41.3 MB (9.69%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -1,14 +1,6 @@
 class Solution {
     public int smallestNumber(int n) {
-        if(n == 1) return 1;
-        int res = n;
-        for(int i = 1; ; i++) {
-            int x = (int) Math.pow(2, i);
-            if(x - 1 >= n) {
-                res = x - 1;
-                break;
-            }
-        }
-        return res;
+        int i = 32 - Integer.numberOfLeadingZeros(n);
+        return (1 << i) - 1;
     }
 }
