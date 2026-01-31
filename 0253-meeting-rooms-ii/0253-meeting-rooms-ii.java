495e7d99dc4cb74dab8bc67716fc8196d43cf326
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2026-01-31 17:01:42 +0530

    Time: 8 ms (94.18%), Space: 48.4 MB (66.26%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        for(int[] curr : intervals) {
+            if(!pq.isEmpty() && pq.peek()[1] <= curr[0]) {
+                pq.poll();
+            }
+            pq.add(curr);
+        }
+        return pq.size();
+    }
+}
