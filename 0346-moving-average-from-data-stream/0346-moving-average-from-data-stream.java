a588fac3e0bb711450ecf1f3328293050cdde44f
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2026-02-04 15:42:45 +0530

    Time: 58 ms (38.72%), Space: 52.3 MB (22.56%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.java
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.java
@@ -1,31 +1,21 @@
 class MovingAverage {
     int k;
     int res;
-    // List<Integer> resl;
     Deque<Integer> dq;
 
     public MovingAverage(int size) {
         this.k = size;
-        // this.resl = new ArrayList<>();
         this.dq = new ArrayDeque<>();
     }
     
     public double next(int val) {
-        // double avg = 0.0;
-        // resl.add(val);
         dq.addLast(val);
         res += val;
 
-        // if(resl.size() > k) 
         if(dq.size() > k) {
-            // res -= resl.get(0);
-            // resl.remove(0);
             res -= dq.removeFirst();
         }
 
-        // avg = (double) res/resl.size();
-        // return avg;
-        // return (double) res/resl.size();
         return (double) res/dq.size();
     }
 }
