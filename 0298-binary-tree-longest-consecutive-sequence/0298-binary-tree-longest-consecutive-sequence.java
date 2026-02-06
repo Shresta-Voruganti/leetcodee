8cd27b2e740e2dd4a976f87504a248557edcb218
VORUGANTI SHRESTA <147568988+Shresta-Voruganti@users.noreply.github.com>
2026-02-06 15:55:21 +0530

    Time: 1 ms (88.7%), Space: 47.5 MB (8.23%) - LeetHub

---
+++ 0298-binary-tree-longest-consecutive-sequence/0298-binary-tree-longest-consecutive-sequence.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxlen = 0;
+    public int longestConsecutive(TreeNode root) {
+        int len = 0;
+        traverse(root, 0, root != null ? root.val - 1 : 0);
+        return maxlen;
+    }
+
+    private void traverse(TreeNode node, int currlen, int parentval) {
+        if(node == null ) {
+            return;
+        }
+
+        if(node.val == parentval + 1) {
+            currlen++;
+        }
+        else {
+            currlen = 1;
+        }
+
+        maxlen = Math.max(maxlen, currlen);
+
+        traverse(node.right, currlen, node.val);
+        traverse(node.left, currlen, node.val);
+    }
+}
